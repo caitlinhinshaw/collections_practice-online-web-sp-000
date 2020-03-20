2fd2669660e010e1d12b03e44456b1cb0d8d2734
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2020-03-20 02:11:58 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -41,5 +41,11 @@ def sum_array(array)
 end
 
 def add_s(array)
-
+  array.each_with_index.collect do |element, index|
+    if index != 1
+      element + "s"
+    else
+      element
+    end
+  end
 end
