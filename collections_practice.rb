b985c947c10ea7898636802eede759986e8b6e81
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2020-03-20 01:55:47 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,11 +15,12 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  second = array[1]
-  third = array[2]
-  array[1] = third
-  array[2] = second
-  array
+  # second = array[1]
+  # third = array[2]
+  # array[1] = third
+  # array[2] = second
+  # array
+  array[1], array[2] = array[2], array[1]
 end
 
 def reverse_array(array)
